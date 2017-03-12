ff986c61f2b492474325d6011bc6090ca45058c2
OliverTheLove <oliver.nyholm@gmail.com>
2017-03-12 22:44:41 +0100

    Remade level2, made Banana more consistent with extra edge collider and fixed bug in FallDownDeath checking for PCG Script

--- Assets/Scripts/FallDownDeath.cs
+++ Assets/Scripts/FallDownDeath.cs
@@ -1,19 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FallDownDeath : MonoBehaviour {
+public class FallDownDeath : MonoBehaviour
+{
 
     PlayerController player;
     GE_LevelGenerator levelGen;
-    public Vector2 startPosition;
+    public Vector3 startPosition;
 
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<PlayerController>();
         startPosition = player.transform.position;
 
-        levelGen = GameObject.Find("CoolGELevel").GetComponent<GE_LevelGenerator>();
-        startPosition = levelGen.GetStartPosition();
+        if (GameObject.Find("CoolGELevel"))
+        {
+            levelGen = GameObject.Find("CoolGELevel").GetComponent<GE_LevelGenerator>();
+            startPosition = levelGen.GetStartPosition();
+            
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col)
